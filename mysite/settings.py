b34269a5a08842b13da09abc2fb68f6891e44f64
sasta.tr <sashanowoe1@gmail.com>
2017-09-25 13:07:48 +0300

    add allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+%$m9ops^cfkpixtg60f&neszy3563^fmj9+cu*_=39lnipui5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sashatr.pythonanywhere.com',]
 
 
 # Application definition
